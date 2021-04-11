f94133056424c9ba407b8fea503166af7c539420
jpramassini <jpramassini@gmail.com>
2021-04-10 22:45:25 -0400

    Added pieces and initial board layout.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+mod pieces;
 use bevy::prelude::*;
+use pieces::*;
 
 // From: https://caballerocoll.com/blog/bevy-chess-tutorial/
 
@@ -14,27 +16,18 @@ fn main() {
     })
     .add_plugins(DefaultPlugins)
     .add_startup_system(setup.system())
+    .add_startup_system(create_board.system())
+    .add_startup_system(create_pieces.system())
     .run();
 }
 
-fn setup(
-    commands: &mut Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+fn setup(commands: &mut Commands) {
     commands
-        // Plane for board
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Plane { size: 16.0 })),
-            material: materials.add(Color::rgb(1., 0.9, 0.9).into()),
-            transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
-            ..Default::default()
-        })
         // Camera
         .spawn(Camera3dBundle {
             transform: Transform::from_matrix(Mat4::from_rotation_translation(
                 Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
-                Vec3::new(-7.0, 20.0, 4.0),
+                Vec3::new(-5.0, 15.0, 4.0),
             )),
             ..Default::default()
         })
@@ -43,4 +36,202 @@ fn setup(
             transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
             ..Default::default()
         });
+}
+
+fn create_board(
+    commands: &mut Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mesh = meshes.add(Mesh::from(shape::Plane { size: 1.0 }));
+    let white_material = materials.add(Color::rgb(1.0, 0.9, 0.9).into());
+    let black_material = materials.add(Color::rgb(0.0, 0.1, 0.1).into());
+
+    for i in 0..8 {
+        for j in 0..8 {
+            commands.spawn(PbrBundle {
+                mesh: mesh.clone(),
+                material: if (i + j + 1) % 2 == 0 {
+                    white_material.clone()
+                } else {
+                    black_material.clone()
+                },
+                transform: Transform::from_translation(Vec3::new(i as f32, 0.0, j as f32)),
+                ..Default::default()
+            });
+        }
+    }
+}
+
+fn create_pieces(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Load all the meshes
+    let king_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh0/Primitive0");
+    let king_cross_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh1/Primitive0");
+    let pawn_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh2/Primitive0");
+    let knight_1_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh3/Primitive0");
+    let knight_2_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh4/Primitive0");
+    let rook_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh5/Primitive0");
+    let bishop_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh6/Primitive0");
+    let queen_handle: Handle<Mesh> =
+        asset_server.load("models/chess_kit/pieces.glb#Mesh7/Primitive0");
+
+    let white_material = materials.add(Color::rgb(1.0, 0.8, 0.8).into());
+    let black_material = materials.add(Color::rgb(0.0, 0.2, 0.2).into());
+
+    // NOTE for Vec3 Positions: X is the rank, Z is the file.
+
+    /*
+     * Setup White Pieces
+     */
+    spawn_rook(
+        commands,
+        white_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(0.0, 0.0, 0.0),
+    );
+
+    spawn_knight(
+        commands,
+        white_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(0.0, 0.0, 1.0),
+    );
+
+    spawn_bishop(
+        commands,
+        white_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(0.0, 0.0, 2.0),
+    );
+
+    spawn_queen(
+        commands,
+        white_material.clone(),
+        queen_handle.clone(),
+        Vec3::new(0.0, 0.0, 3.0),
+    );
+
+    spawn_king(
+        commands,
+        white_material.clone(),
+        king_handle.clone(),
+        king_cross_handle.clone(),
+        Vec3::new(0.0, 0.0, 4.0),
+    );
+
+    spawn_bishop(
+        commands,
+        white_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(0.0, 0.0, 5.0),
+    );
+
+    spawn_knight(
+        commands,
+        white_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(0.0, 0.0, 6.0),
+    );
+
+    spawn_rook(
+        commands,
+        white_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(0.0, 0.0, 7.0),
+    );
+
+    // Set up white pawns
+    for i in 0..8 {
+        spawn_pawn(
+            commands,
+            white_material.clone(),
+            pawn_handle.clone(),
+            Vec3::new(1.0, 0.0, i as f32),
+        )
+    }
+
+    /*
+     * Setup Black Pieces
+     */
+    spawn_rook(
+        commands,
+        black_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(7.0, 0.0, 0.0),
+    );
+
+    spawn_knight(
+        commands,
+        black_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(7.0, 0.0, 1.0),
+    );
+
+    spawn_bishop(
+        commands,
+        black_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(7.0, 0.0, 2.0),
+    );
+
+    spawn_queen(
+        commands,
+        black_material.clone(),
+        queen_handle.clone(),
+        Vec3::new(7.0, 0.0, 3.0),
+    );
+
+    spawn_king(
+        commands,
+        black_material.clone(),
+        king_handle.clone(),
+        king_cross_handle.clone(),
+        Vec3::new(7.0, 0.0, 4.0),
+    );
+
+    spawn_bishop(
+        commands,
+        black_material.clone(),
+        bishop_handle.clone(),
+        Vec3::new(7.0, 0.0, 5.0),
+    );
+
+    spawn_knight(
+        commands,
+        black_material.clone(),
+        knight_1_handle.clone(),
+        knight_2_handle.clone(),
+        Vec3::new(7.0, 0.0, 6.0),
+    );
+
+    spawn_rook(
+        commands,
+        black_material.clone(),
+        rook_handle.clone(),
+        Vec3::new(7.0, 0.0, 7.0),
+    );
+
+    // Set up white pawns
+    for i in 0..8 {
+        spawn_pawn(
+            commands,
+            black_material.clone(),
+            pawn_handle.clone(),
+            Vec3::new(6.0, 0.0, i as f32),
+        )
+    }
 }
